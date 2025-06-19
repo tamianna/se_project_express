8cf769008d79f7cacb6b2d70368b9e7be800b3f2
Tami Hernandez <tamianna.h@gmail.com>
2025-06-19 17:49:51 -0500

    feat: add app.get to test page, commit out lines 6-8.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
+//app.get('/', (req, res) => {
+//  res.send('Hello from Express!');
+//});
 
 app.listen(PORT, () => {
   console.log('Server is running on port ${PORT}');
