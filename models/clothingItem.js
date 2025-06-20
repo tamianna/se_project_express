e6cf9d24cfa098c5dbc93c36fe16cbdc9291c5c4
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 09:36:52 -0500

    feat: create clothingItemSchema in clothingItem.js, in models folder.

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
+const clothingItemSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
-  avatar: {
+  weather: {
+    type: String,
+    required: true,
+    enum: ['hot', 'warm', 'cold'],
+  },
+  imageUrl: {
     type: String,
     required: true,
     validate: {
@@ -18,6 +23,20 @@ const userSchema = new mongoose.Schema({
       message: 'You must enter a valid URL',
     },
   },
+  owener: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('ClothingItem', clothingItemSchema);
