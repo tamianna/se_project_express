186898de68402b1288ba26a18df7ca21e732d291
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 10:08:01 -0500

    feat: create router in clothingItems.js, in routes folder.

---
+++ routes/clothingItems.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getClothingItems, createClothingItem, deleteClothingItem } = require('../controllers/clothingItems');
+
+router.get('/', getClothingItems);
+router.post('/', createClothingItem);
+router.delete('/:clothingItemId', deleteClothingItem);
+
+module.exports = router;
