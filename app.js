7a09d749bc91e8548c6b14229b35c5912f7b5f47
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 09:21:28 -0500

    feat: install and connect mongoose in app.js.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const { PORT = 3001 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 
 //app.get('/', (req, res) => {
 //  res.send('Hello from Express!');
