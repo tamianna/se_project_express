99ae440b6e4be4c4c690c131677254cb01b96bec
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 10:13:15 -0500

    feat: import routes into app.js and apply.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 const { PORT = 3001 } = process.env;
 
+const userRoutes = require('./routes/users');
+const clothingItemRoutes = require('./routes/clothingItems');
+
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/clothingItems', clothingItemRoutes);
 
 //app.get('/', (req, res) => {
 //  res.send('Hello from Express!');
