82771db9217b15218f7d6b311a807fdabb554a14
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 09:50:18 -0500

    feat: create getUsers, getUser & createUser in users.js, in controllers folder.

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+const getUsers = async (req, res) => {
+  const users = await User.find({});
+  res.send(users);
+};
+
+const gerUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(404).send({ message: 'User not found' });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send({ message: 'Invalid user ID' });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { name, avatar } = req.body;
+    const user = await User.create({ name, avatar });
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send({ message: 'Invalid user data' });
+  }
+};
+
+module.exports = { getUsers, getUser, createUser };
