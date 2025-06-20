b68434f01468b92671fc4e1b386bcbb234e48480
Tami Hernandez <tamianna.h@gmail.com>
2025-06-20 11:57:39 -0500

    refactor: import and apply error.js in user.js... in controllers folder.

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,57 @@
 const User = require("../models/user");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 
 const getUsers = async (req, res) => {
-  const users = await User.find({});
-  res.send(users);
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occured on the server" });
+    });
 };
 
 const getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    if (!user) return res.status(404).send({ message: "User not found" });
-    res.send(user);
-  } catch (err) {
-    res.status(400).send({ message: "Invalid user ID" });
-  }
+  User.findById(req.params.userId)
+    .orFail(() => {
+      const error = new Error("User not found");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((user) => res.send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid user ID" });
+      }
+      res.status(err.statusCode || INTERNAL_SERVER_ERROR).send({
+        message:
+          err.statusCode === NOT_FOUND
+            ? err.message
+            : "An error has occurred on the server",
+      });
+    });
 };
 
 const createUser = async (req, res) => {
-  try {
-    const { name, avatar } = req.body;
-    const user = await User.create({ name, avatar });
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send({ message: "Invalid user data" });
-  }
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid user data" });
+      }
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
 };
 
 module.exports = { getUsers, getUser, createUser };
