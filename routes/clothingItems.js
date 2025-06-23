a0ea1e3ca3519f3c60615d4735edd26d70f5f30f
Tami Hernandez <tamianna.h@gmail.com>
2025-06-23 08:31:41 -0500

    feat: add like & dislike to clothingItems.js, in routes folder.

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -4,10 +4,15 @@ const {
   getClothingItems,
   createClothingItem,
   deleteClothingItem,
+  likeClothingItem,
+  dislikeClothingItem,
 } = require("../controllers/clothingItems");
 
 router.get("/", getClothingItems);
 router.post("/", createClothingItem);
 router.delete("/:clothingItemId", deleteClothingItem);
+
+router.put("/:clothingItemId/likes", likeClothingItem);
+router.delete("/:clothingItemId/likes", dislikeClothingItem);
 
 module.exports = router;
