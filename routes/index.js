90031773221f6c0a176e38ec8ae8e8d08150d975
Tami Hernandez <tamianna.h@gmail.com>
2025-06-26 16:04:55 -0500

    feat: create routes users/items in index.js.

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const userRoutes = require(".users/users");
+const clothingItemRoutes = require("./clothingItems");
+
+router.user("/users", userRoutes);
+router.use("/items", clothingItemRoutes);
+
+module.exports = router;
