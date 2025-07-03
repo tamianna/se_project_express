23e60f68a52b1c3e179a29e79d251d1e9595c3a4
Tami Hernandez <tamianna.h@gmail.com>
2025-07-03 12:00:19 -0500

    feat: install cors. import cors, login, createUser and auth into app.js.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 
 const routes = require("./routes/index");
 const { NOT_FOUND } = require("./utils/errors");
+const { login, createUser } = require("./controllers/users");
+const auth = require("./middlewares/auth");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
