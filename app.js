03f987cc4a42339303d5fa2149f84f22ea6d01fd
Tami Hernandez <tamianna.h@gmail.com>
2025-07-03 12:03:14 -0500

    refactor: implament cors, login, createUser & auth in app.js. delete hard coded user object.

--- app.js
+++ app.js
@@ -13,20 +13,13 @@ const auth = require("./middlewares/auth");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "68558513705d7d68477548aa",
-  };
-  next();
-});
+app.post("/signin", login);
+app.post("/signup", createUser);
 
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.path}`);
-  next();
-});
-
+app.use(auth);
 app.use("/", routes);
 
 app.use("*", (req, res) => {
