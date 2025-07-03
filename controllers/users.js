5b3eb5adc4c52ecea41eddc15dee040bc375ecce
Tami Hernandez <tamianna.h@gmail.com>
2025-07-03 11:10:16 -0500

    feat: merge config/updateErrors branch. import bcrypt, jwt, JWT_SECRET, CONFLICT, UNAUTHORIZED in users.js, in folder controllers. create const login. refactor createUser.

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,13 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
 const {
   BAD_REQUEST,
   NOT_FOUND,
   INTERNAL_SERVER_ERROR,
+  CONFLICT,
+  UNAUTHORIZED,
 } = require("../utils/errors");
 
 const getUsers = (req, res) =>
@@ -14,6 +19,41 @@ const getUsers = (req, res) =>
         .status(INTERNAL_SERVER_ERROR)
         .send({ message: "An error has occured on the server" });
     });
+
+const createUser = (req, res) => {
+  const { name, avatar, email, password } = req.body;
+  bcrypt.hash(password, 10)
+  .then((hash) => User.create({ name, avatar, email, password: hash }))
+  .then((user) => {
+    const userData = user.toObject();
+    delete userData.password;
+    res.status(201).send(userData);
+  })
+    .catch((err) => {
+      if (err.code === 11000) {
+        return res.status(CONFLICT).send({ message: "Email already exists"
+        });
+      }
+      if (err.name === "ValidationError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid user data" });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
+    });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findByCredentials(email, password)
+  .then((user) => {
+    const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
+    res.send({ token });
+  })
+  .catch(() =>{
+    res.status(UNAUTHORIZED).send({ message: "Incorrect email or password" });
+  });
+}
 
 const getUser = (req, res) =>
   User.findById(req.params.userId)
@@ -35,21 +75,5 @@ const getUser = (req, res) =>
             : "An error has occurred on the server",
       });
     });
-
-const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  return User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: "Invalid user data" });
-      }
-      return res
-        .status(INTERNAL_SERVER_ERROR)
-        .send({ message: "An error has occurred on the server" });
-    });
-};
 
 module.exports = { getUsers, getUser, createUser };
