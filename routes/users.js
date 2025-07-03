d00f89c713c28a5510fbc10ee260a739684b6895
Tami Hernandez <tamianna.h@gmail.com>
2025-07-03 11:45:48 -0500

    refactor: import getCurrentUser & updateUser in user.js. replace router.get with getCurrentUser. feat: create router.patch with updateUser. in folder routes.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const { getUsers, getUser, createUser } = require("../controllers/users");
+const { getCurrentUser, updateUser } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-router.post("/", createUser);
+router.get("/me", getCurrentUser);
+router.patch("/me", updateUser);
 
 module.exports = router;
