b7c63f3908aa24ad99f31812a95be6c92caca981
Tami Hernandez <tamianna.h@gmail.com>
2025-07-10 08:42:18 -0500

    refactor: removed login & createUser post routes from app.js and add them into users.js routes file.

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ const { PORT = 3001 } = process.env;
 
 const routes = require("./routes/index");
 const { NOT_FOUND } = require("./utils/errors");
-const { login, createUser } = require("./controllers/users");
 const auth = require("./middlewares/auth");
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(cors());
 app.use(express.json());
-
-app.post("/signin", login);
-app.post("/signup", createUser);
 
 app.use(auth);
 app.use("/", routes);
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
-const { getCurrentUser, updateUser } = require("../controllers/users");
+const {
+  getCurrentUser,
+  updateUser,
+  login,
+  createUser,
+} = require("../controllers/users");
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.get("/me", getCurrentUser);
 router.patch("/me", updateUser);
