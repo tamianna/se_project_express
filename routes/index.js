d1771e2fc352feef4d4c5367522a1cc1adf35b5c
Tami Hernandez <tamianna.h@gmail.com>
2025-07-11 12:48:18 -0500

    refactor: removed routes 'signin' & 'signup' from routes/users.js and add them to routes/index.js.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const userRoutes = require("./users");
 const clothingItemRoutes = require("./clothingItems");
 
+const { login, createUser } = require("../controllers/users");
+
 const router = express.Router();
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use("/users", userRoutes);
 router.use("/items", clothingItemRoutes);
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,12 @@
 const express = require("express");
 
 const router = express.Router();
-const {
-  getCurrentUser,
-  updateUser,
-  login,
-  createUser,
-} = require("../controllers/users");
+const { getCurrentUser, updateUser } = require("../controllers/users");
 
 const auth = require("../middlewares/auth");
 
-router.post("/signin", login);
-router.post("/signup", createUser);
-
 router.use(auth);
+
 router.get("/me", getCurrentUser);
 router.patch("/me", updateUser);
 
