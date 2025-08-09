41764923ecbb8a91bd566e6036e419399877133a
Tami Hernandez <tamianna.h@gmail.com>
2025-08-09 17:38:11 -0500

    feat: disable rule for next in .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    'no-underscore-dangle': ['error', { allow:['_id'] }],
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
   },
 };
